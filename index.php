17eff0fa3128f9f3ac5610c1517bcef744c5bf67
Eugene-L-H <eugenehelland@mail.com>
2023-09-08 12:54:08 -0600

    Pizzas from the database render to index page in the form of boxes.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (!$connection) {
 }
 
 // query for all pizzas
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 // make query & get results
 
@@ -17,8 +17,6 @@ $result = mysqli_query($connection, $sql);
 
 // store the resulting rows as an array
 $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-print_r($pizzas);
 
 // free result from memory
 mysqli_free_result($result);
@@ -30,7 +28,35 @@ mysqli_close($connection)
 
 <!DOCTYPE html>
 <html lang="en">
+
 <?php include('templates/header.php') ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+  <div class="row">
+    <?php foreach ($pizzas as $pizza) { ?>
+
+      <div class="col s6 md3 ">
+        <div class="card z-depth-0">
+          <div class="card-content center">
+            <h6>
+              <?php echo htmlspecialchars($pizza['title']) ?>
+            </h6>
+            <div>
+              <?php echo htmlspecialchars($pizza['ingredients']) ?>
+            </div>
+          </div>
+          <div class="card-action right-align">
+            <a href="#" class="brand-text">more info</a>
+          </div>
+        </div>
+      </div>
+
+    <?php } ?>
+  </div>
+</div>
+
 <?php include('templates/footer.php') ?>
 
 </html>
