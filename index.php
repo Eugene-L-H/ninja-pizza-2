7e1a4d16954bee5c3a1aacc5702e954a32a0a609
Eugene-L-H <eugenehelland@mail.com>
2023-09-08 11:28:19 -0600

    index.php queries database for pizzas, and stores them as an associative array.

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php
 
 // connect to database
-
 $connection = mysqli_connect('localhost', 'eugene', '3434', 'ninja_pizza');
 
 // check connection
-
 if (!$connection) {
   'Error connecting to database.' . mysqli_connect_error();
 }
 
-?>
+// query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+// make query & get results
+
+$result = mysqli_query($connection, $sql);
+
+// store the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+print_r($pizzas);
+
+// free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($connection)
+
+  ?>
 
 <!DOCTYPE html>
 <html lang="en">
